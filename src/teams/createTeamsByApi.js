d4fc15b180015b381b3cbe1fc6f513aab421e71b
JongHoo <monkeis7@gmail.com>
2019-07-02 22:27:34 +0900

    lodash 추가, 팀 목록 에러처리 추가

--- src/teams/createTeamsByApi.js
+++ src/teams/createTeamsByApi.js
@@ -1,25 +1,32 @@
 const commonUtil = require('../common/commonUtil')
 const Team = require('../models/Team')
 const axios = require('axios')
+const _ = require('lodash')
 
 exports.handle = (event, ctx, cb) => {
   ctx.callbackWaitsForEmptyEventLoop = false
   let teamListByApi = [] //JSON.parse(event.body)
+  let teamList = []
   const { league, season } = event.pathParameters
   axios.get(`http://soccer.sportsopendata.net/v1/leagues/${league}/seasons/${season}/teams`)
     .then((res) => {
       console.log('response teams : ', res.data.data.teams)
+      if (_.isEmpty(res.data.data.teams)) {
+        throw new Error('no teams')
+      }
       teamListByApi = res.data.data.teams
       return commonUtil.connect()
     })
     .then(() => {
-      let teamList = []
       teamListByApi.forEach(item => {
         item.league = league
         item.season = season
         let team = new Team(item)
         teamList.push(team)
       })
+      return Team.deleteMany({league: league, season: season})
+    })
+    .then(() => {
       return Team.create(teamList)
     })
     .then(data => {
@@ -30,4 +37,3 @@ exports.handle = (event, ctx, cb) => {
       cb(err)
     })
 }
-
