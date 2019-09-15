4e08d087dd4b73c45bca8df32cdb2581d79358a2
JongHoo <monkeis7@gmail.com>
2019-09-15 17:10:26 +0900

    대시보드용 read top standing 추가

--- src/standings/readTopStandings.js
+++ src/standings/readTopStandings.js
@@ -3,14 +3,15 @@ const Standing = require('../models/Standing')
 
 exports.handle = (event, ctx, cb) => {
   ctx.callbackWaitsForEmptyEventLoop = false
-  const { league, season } = event.pathParameters
+  const { season } = event.pathParameters
   commonUtil.connect()
     .then(() => {
       return Standing.find()
-        .where('league_id').equals(league)
         .where('season').equals(season)
+        .lte('position', 4)
+        .sort('league_id')
         .sort('position')
-        .select('league_id season position team wins draws losts points scores conceded matches_played goal_difference')
+        .select('-_id league_id season position team wins draws losts points scores conceded matches_played goal_difference')
     })
     .then((standingList) => {
       cb(null, commonUtil.createResponse(200, standingList))
